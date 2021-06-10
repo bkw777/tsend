573a256276251d16f94ed05e30dbd978689a80a6
Brian K. White <54458165+bkw777@users.noreply.github.com>
2021-06-10 16:22:51 -0400

    progress indicator

--- tsend.ps1
+++ tsend.ps1
@@ -1,5 +1,5 @@
 # tsend.ps1
-# Powershell implementation of a bootstrapper for "Model T" computers.
+# Powershell implementation of a bootstrapper for "Model T Computers"
 #
 # Reads a local file and writes it out to a serial port, one byte at a time
 # with a 6ms pause after each byte, and sends a trailing Ctrl-Z at the end.
@@ -7,7 +7,9 @@
 # Usage (example):
 # .\tsend.ps1 -port COM5 -file TS-DOS.100
 #
-# -port may be omitted if there is only one serial port present
+# If port is omitted, and there is only one serial port detected, it is used
+# If port is omitted, and there are more than one serial ports detected, the detected ports are displayed and the user is prompted to select one.
+# If file is omitted, the user is prompted to supply a flename.
 
 param (
 	[string]$port,
@@ -15,7 +17,7 @@ param (
 )
 $char_delay_ms = 6
 
-# Serial port
+# serial port
 if($port -eq ""){
 	[string[]]$ports = [System.IO.Ports.SerialPort]::getportnames()
 	if($ports.count -lt 1) {
@@ -43,22 +45,28 @@ if($file -eq ""){
 
 Write-Host ""
 Write-Host "Prepare the portable to receive. Hints:"
-Write-Host "	RUN `"COM:98N1ENN`"		# TRS-80, TANDY, Kyotronic, Olivetti"
-Write-Host "	RUN `"COM:9N81XN`"		# NEC"
+Write-Host "	RUN `"COM:98N1ENN`"	# for TRS-80, TANDY, Kyotronic, Olivetti"
+Write-Host "	RUN `"COM:9N81XN`"	# for NEC"
 Write-Host ""
 Read-Host "Press Enter when the portable is ready..."
 
-[byte[]] $bytes = Get-Content $file -encoding byte -readcount 0
 $p = new-Object System.IO.Ports.SerialPort $port,19200,None,8,one
 $p.open()
 
+$x = 0
+$i = 0
+[byte[]] $bytes = Get-Content $file -encoding byte -readcount 0
 foreach ($byte in $bytes) {
-	$p.Write($byte,0,1)
-	Write-Host "." -nonewline
+	$i++
+	$p.write($byte,0,1)
+	if($x++ -gt 99){
+		$pc = [math]::round(($i/$bytes.count)*100)
+		Write-Progress -Activity "Sending" -PercentComplete $pc
+		$x = 0
+	}
 	Start-Sleep -milliseconds $char_delay_ms
 }
 
-$p.Write([byte[]]0x1A,0,1)
-Write-Host ""
+$p.write([byte[]]0x1A,0,1)
 
-$p.Close()
+$p.close()
